fc2d3cf4182c60f06a4af91613694d4cab7f59dc
Adam Merrifield <macagp@gmail.com>
2014-10-11 14:59:16 -0400

    Added base for php server.

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,7 +69,8 @@ module.exports = function (grunt) {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
-        livereload: 35729
+        livereload: 35729,
+        base: '<%= yeoman.app %>'
       },
       livereload: {
         options: {
