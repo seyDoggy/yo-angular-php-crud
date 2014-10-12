97e01d44c2f12eaa2d66034803e56f918de12606
Adam Merrifield <macagp@gmail.com>
2014-10-12 07:58:51 -0400

    Added cars controller handlers.

--- app/scripts/controllers/cars.js
+++ app/scripts/controllers/cars.js
@@ -2,16 +2,81 @@
 
 /**
  * @ngdoc function
- * @name myWebAppApp.controller:CarsCtrl
+ * @name yoAngularPhpCrudApp.controller:CarsCtrl
  * @description
  * # CarsCtrl
- * Controller of the myWebAppApp
+ * Controller of the yoAngularPhpCrudApp
  */
-angular.module('myWebAppApp')
-  .controller('CarsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+angular.module('yoAngularPhpCrudApp')
+  .controller('CarsCtrl', function ($scope, Restangular) {
+      var url = 'cars';
+      var Cars = Restangular.all(url);
+      var blankCar = {
+          make:"",
+          model:"",
+          pkg:"",
+          year:""
+      };
+      var refreshCars = function() {
+          $scope.isEditVisible = false;
+          $scope.isAddNewCar = false;
+          Cars.getList().then(function(data) {
+              console.log('--> api/cars called from refreshCars()');
+              $scope.cars = data;
+          });
+      };
+
+      $scope.showAdd = function () {
+          if ($scope.isEditVisible === false) {
+              $scope.isAddNewCar = true;
+          }
+      };
+
+      $scope.hideAdd = function () {
+          $scope.isAddNewCar = false;
+          $scope.newCar = angular.copy($scope.blankCar);
+      };
+
+      $scope.showEdit = function (idx) {
+          $scope.isEditVisible = idx;
+          $scope.newCar = angular.copy(blankCar);
+      };
+
+      $scope.saveCar = function() {
+          $scope.isEditVisible = false;
+          $scope.isAddNewCar = false;
+          Cars.post($scope.newCar).then(function (data) {
+              $scope.cars.push(data);
+              console.log('--> newCar saved!');
+          },
+          function () {
+              console.log('--> Could not save newCar!');
+          });
+      };
+
+      $scope.updateCar = function(idx) {
+          $scope.isAddNewCar = false;
+          Cars.getList().then(function(data) {
+              console.log('--> api/cars called from updateCar()');
+              $scope.cars = data;
+              var carWithId = _.find($scope.cars, function(car) {
+                  return car.id === $scope.cars[idx].id;
+              });
+
+              carWithId.make = $scope.newCar.make || carWithId.make;
+              carWithId.model = $scope.newCar.model || carWithId.model;
+              carWithId.pkg = $scope.newCar.pkg || carWithId.pkg;
+              carWithId.year = $scope.newCar.year || carWithId.year;
+              carWithId.put();
+              $scope.isEditVisible = false;
+          });
+      };
+
+      $scope.deleteCar = function(idx) {
+          var car = Restangular.one(url, $scope.cars[idx].id);
+          car.remove();
+          $scope.cars.splice(idx, 1);
+      };
+
+      refreshCars();
   });
